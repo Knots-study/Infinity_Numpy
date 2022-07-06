98faf58ee36bb6987caf624c45c235f22b21e036
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2022-07-06 22:31:04 +0900

    commit

--- Q6.py
+++ Q6.py
@@ -2,6 +2,9 @@ anko_a_name = ["こしあん","ごまあん","栗あん","かぼちゃあん"]
 anko_b_name = ["大福","大福","あんぱん","あんぱん"]
 anko_b = []
 
+#ハードコーディング:ベタ打ち
+#せっかくなのでzipをつかった
+
 for a,b in zip(anko_a_name,anko_b_name):
     anko_b.append([a,b])
 
