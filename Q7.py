62101a3df54c8366b11e695249801907f5a986d9
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2022-07-06 22:35:16 +0900

    commit

---
+++ Q7.py
@@ -0,0 +1,10 @@
+anko_a_name = ["こしあん","ごまあん","栗あん","かぼちゃあん"]
+anko_b_name = ["大福","大福","あんぱん","あんぱん"]
+anko_b = []
+
+for a,b in zip(anko_a_name,anko_b_name):
+    anko_b.append([a,b])
+
+for anko in anko_b:
+    for num in range(2):
+        print(anko[num],end="---")
