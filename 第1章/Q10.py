9fc5a215ee29fcfcfa071c25d43ede105ac708fb
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2022-07-06 22:52:32 +0900

    commit

---
+++ 第1章/Q10.py
@@ -0,0 +1,12 @@
+anko_a_name = ["こしあん","ごまあん","栗あん","かぼちゃあん"]
+anko_b_name = ["大福","大福","あんぱん","あんぱん"]
+anko_b = []
+
+for a,b in zip(anko_a_name,anko_b_name):
+    anko_b.append([a,b])
+
+for anko in anko_b:
+    if(anko[1] == "あんぱん"):
+        for item in anko:
+            print(item,end =" ")
+        print()
